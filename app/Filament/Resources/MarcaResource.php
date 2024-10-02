cb58ad71dd1d9bb89b3e819e7b2af87804471be9
Diego Ochoa <one.diego@gmail.com>
2024-10-01 21:56:56 -0300

    Marca resource creada

---
+++ app/Filament/Resources/MarcaResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MarcaResource\Pages;
+use App\Filament\Resources\MarcaResource\RelationManagers;
+use App\Models\Marca;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MarcaResource extends Resource
+{
+    protected static ?string $model = Marca::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('nombre')
+                    ->placeholder('Ingrese el nombre de la marca')
+                    ->required()
+                    ->maxLength(191)
+                    ->unique(Marca::class, 'nombre', ignoreRecord: true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nombre')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Fecha de registro')
+                    ->dateTime('d-m-Y H:m:s')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Fecha de actualización')
+                    ->dateTime('d-m-Y H:m:s')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ]),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMarcas::route('/'),
+            'create' => Pages\CreateMarca::route('/create'),
+            'edit' => Pages\EditMarca::route('/{record}/edit'),
+        ];
+    }
+}
